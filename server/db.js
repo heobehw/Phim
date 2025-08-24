52e6567a9a994195e7c7d1ce91209ab59cd19022
heobehw <huybui161200@hotmail.com>
2025-08-24 14:55:24 +0700

    Delete server directory

--- server/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect('mongodb://localhost:27017/xemphim', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
